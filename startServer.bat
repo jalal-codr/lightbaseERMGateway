8c186e140304ac6beb65030a241a6d3a6ea6bf9e
jalal-codr <abduljalal849@gmail.com>
2026-03-12 14:25:43 +0100

    fix: Update directory path in startServer.bat for correct execution

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d C:\Documents\lightbaseERMGateway || (echo Failed to find directory & pause & exit /b 1)
+cd /d C:\Users\darle\Documents\lightbaseERMGateway || (echo Failed to find directory & pause & exit /b 1)
 server.exe
