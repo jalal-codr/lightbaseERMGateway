ca9315c45cefdf162db8db2dcd39f5438b35e26b
jalal-codr <abduljalal849@gmail.com>
2026-03-18 16:32:34 +0100

    update

--- startServer.bat
+++ startServer.bat
@@ -1,21 +1,5 @@
 @echo off
 cd /d C:\Users\darle\Documents\lightbaseERMGateway
-
-echo Pulling latest changes...
 git pull
-if %errorlevel% neq 0 (
-    echo Git pull failed. Aborting.
-    pause
-    exit /b 1
-)
-
-echo Building server...
-go build -o server.exe ./cmd/server
-if %errorlevel% neq 0 (
-    echo Build failed. Aborting.
-    pause
-    exit /b 1
-)
-
-echo Starting server...
+go build -o server.exe ./cmd/server 
 start "LightbaseERMGateway" cmd /k server.exe
