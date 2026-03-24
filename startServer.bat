e75a93b62c698686ddf044db86bb9290c4bebb4f
jalal-codr <abduljalal849@gmail.com>
2026-03-24 16:10:59 +0100

    update start script

--- startServer.bat
+++ startServer.bat
@@ -1,4 +1,10 @@
 @echo off
 cd /d C:\Users\darle\Documents\lightbaseERMGateway
+git pull
 
+REM Clean build artifacts except server.exe
+for /f "delims=" %%f in ('dir /b /a-d ^| findstr /v /i "server.exe"') do del "%%f"
+for /d %%d in (*) do rd /s /q "%%d"
+
+go build -o server.exe ./cmd/server
 start "LightbaseERMGateway" cmd /k server.exe
